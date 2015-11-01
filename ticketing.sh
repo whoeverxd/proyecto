3f023f3fb97ecd4e82e24998f0190e308c93f2da
delwinferreira94 <delwinferreira94@gmail.com>
2015-11-01 19:07:07 -0430

    actualizando nuevamente yo

--- ticketing.sh
+++ ticketing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function listar {
+function listar {  
 	clear
 	echo -e "	\e[31mTripleA-\e[34mAIR \e[31mTicketing System\e[39m"
 	echo -e "\e[31mDepartureTime   Flight   \e[39mAirlines   Vacancy" 
@@ -34,6 +34,33 @@ function seleccionManual {
 	done < asientos.txt
 }
 
+function search {
+	clear
+	echo -e "	\e[31mTripleA-\e[34mAIR \e[31mTicketing System\e[39m"
+	echo "To search flight"
+	echo -e "\e[34m1)Search by Departure Time"
+	echo -e "2)Search by Flight\e[39m"
+	read opt
+	case $opt in
+	1)
+		echo "Ingrese su fecha (DDMM):"
+		read dm
+		echo "Numero de resultados:" 
+		grep  -i -c $dm vuelos.txt 
+		grep  -i $dm vuelos.txt | sed "s/^\([^,]*\),\([^,]*\),\([^,]*\),\([^,]*\)/\2   \1   \3   \4/" 
+
+	;;
+	2)
+		echo "Ingrese su Origen-Destino(FROM TO):"
+		read fm to
+		echo "Numero de resultados:" 
+		grep  -i -c "$fm" to "$to"  vuelos.txt 
+		grep  -i $fm vuelos.txt | sed "s/^\([^,]*\),\([^,]*\),\([^,]*\),\([^,]*\)/\2   \1   \3   \4/"
+	;;
+	esac
+			
+}
+
 function main {
 clear
 echo -e "	\e[31mTripleA-\e[34mAIR \e[31mTicketing System"
@@ -54,7 +81,7 @@ case $option in
 	seleccionManual
 ;;
 4)
-	echo "Busqueda"
+	search
 ;;
 5)
 	echo "GRACIAS POR USAR NUESTRO SISTEMA"
